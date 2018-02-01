71e1ee69abe90513904f15d76ba731418bb6f86a
DiDex <didexdida@gmail.com>
2018-02-01 17:48:07 +0800

    add cache to sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
-'use strict';
-var gulp   = require("gulp"),
-	sass     = require("gulp-sass"),
-	concat   = require("gulp-concat"),
-	csso     = require("gulp-csso"),
+"use strict";
+const gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	concat = require("gulp-concat"),
+	csso = require("gulp-csso"),
 	// gulpcopy = require("gulp-copy"),
-	image    = require("gulp-imagemin"),
-	uglify   = require("gulp-uglifyjs"),
-	htmlmin  = require("gulp-htmlmin"),
-	autoprx  = require("gulp-autoprefixer"),
-	browser  = require("browser-sync");
+	image = require("gulp-imagemin"),
+	uglify = require("gulp-uglifyjs"),
+	htmlmin = require("gulp-htmlmin"),
+	autoprx = require("gulp-autoprefixer"),
+	browser = require("browser-sync"),
+	remember = require("gulp-remember"),
+	cached = require("gulp-cached"),
+	debug = require("gulp-debug"),
+	sourcemaps = require("gulp-sourcemaps");
 
-gulp.task("lib:js",function(){
-	return gulp.src([
+
+gulp.task("lib:js", function() {
+	return gulp
+		.src([
 			"app/libs/jquery/dist/jquery.min.js",
-			"app/libs/materialize/materialize.min.js",
+			"app/libs/materialize/materialize.min.js"
 			// "app/libs/bootstrap/dist/js/bootstrap.min.js",
 			// "app/libs/vegas/dist/vegas.min.js",
 			// "app/libs/owl.carousel/dist/owl.carousel.min.js",
@@ -25,43 +31,54 @@ gulp.task("lib:js",function(){
 		.pipe(gulp.dest("app/js"));
 });
 
-gulp.task("lib:css",["sass"], function(){
-	return gulp.src([
-		// "app/libs/magnific-popup/dist/magnific-popup.css",
-		// "app/libs/owl.carousel/dist/assets/owl.carousel.min.css",
-		// "app/libs/vegas/dist/vegas.min.css",
-		// "app/libs/font-awesome/css/font-awesome.css",
-		// "app/libs/materialize/ghpages-materialize.css",
-		// "app/libs/bootstrap/dist/css/bootstrap.css"
+gulp.task("lib:css", ["sass"], function() {
+	return gulp
+		.src([
+			// "app/libs/magnific-popup/dist/magnific-popup.css",
+			// "app/libs/owl.carousel/dist/assets/owl.carousel.min.css",
+			// "app/libs/vegas/dist/vegas.min.css",
+			// "app/libs/font-awesome/css/font-awesome.css",
+			// "app/libs/materialize/ghpages-materialize.css",
+			// "app/libs/bootstrap/dist/css/bootstrap.css"
 		])
 		.pipe(concat("libs.css"))
 		.pipe(csso())
 		.pipe(gulp.dest("app/css"));
 });
 
-gulp.task("libs",["lib:css","lib:js"],function(){return});
-
-gulp.task("sass", function(){
-	return gulp.src("app/sass/*.sass")
-		.pipe(sass())
-		.pipe(autoprx(["last 15 versions","> 1%", "ie 8", "ie 7"], {cascade: true}))
-		.pipe(gulp.dest("app/css/"))
-		.pipe(browser.reload({stream: true}));
+gulp.task("libs", ["lib:css", "lib:js"], function() {
+	return;
 });
 
-gulp.task("sass:stream",function(){
-	return sassruby("app/sass", {sourcemap: true})
+gulp.task("sass", function() {
+	return gulp
+		.src("app/sass/*.sass")
+		.pipe(cached("sass"))
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(
+			autoprx(["last 15 versions", "> 1%"], { cascade: true })
+		)
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("app/css/"))
+		.pipe(browser.reload({ stream: true }));
+});
+
+gulp.task("sass:stream", function() {
+	return sassruby("app/sass", { sourcemap: true })
 		.on("error", function(err) {
 			console.error("Error!", err.message);
 		})
-		.pipe(sourcemap.write("./", {
-			includeContent: false,
-			sourceRoot: "app/sass"
-		}))
-		.pipe(browserSync.stream({match: "**/*.css"}));
+		.pipe(
+			sourcemap.write("./", {
+				includeContent: false,
+				sourceRoot: "app/sass"
+			})
+		)
+		.pipe(browserSync.stream({ match: "**/*.css" }));
 });
 
-gulp.task('browser',function(){
+gulp.task("browser", function() {
 	browser({
 		server: {
 			baseDir: "app"
@@ -70,43 +87,46 @@ gulp.task('browser',function(){
 	});
 });
 
-gulp.task("watch", ["browser", "sass"], function(){
-	gulp.watch("app/sass/**/*.sass",["sass"]);
+gulp.task("watch", ["browser", "sass"], function() {
+	gulp.watch("app/sass/**/*.sass", ["sass"]);
 	gulp.watch("app/*.html", browser.reload);
 	gulp.watch("app/js/**/*.js", browser.reload);
 });
 
-gulp.task("watch:dist", ["build","browser"], function(){
+gulp.task("watch:dist", ["build", "browser"], function() {
 	gulp.watch("dist/*.html", browser.reload);
 });
 
-gulp.task("build" , function(){
-	gulp.src("app/img/**/*")
+gulp.task("build", function() {
+	gulp
+		.src("app/img/**/*")
 		.pipe(image())
 		.pipe(gulp.dest("dist/img"));
-	gulp.src("app/css/**/*")
+	gulp
+		.src("app/css/**/*")
 		.pipe(csso())
 		.pipe(gulp.dest("dist/css"));
-	gulp.src("app/js/**/*")
+	gulp
+		.src("app/js/**/*")
 		.pipe(uglify())
 		.pipe(gulp.dest("dist/js"));
-	gulp.src("app/fonts/**/*")
-		.pipe(gulp.dest("dist/fonts"));
-	return gulp.src("app/*.html")
-		// .pipe(htmlmin({
-		// 		collapseWhitespace: "true",
-		// 		removeAttributeQuotes: "true",
-		// 		removeComments: "true",
-		// 		removeEmptyAttributes: "true",
-		// 		removeEmptyElements: "true",
-		// 		removeOptionalTags: "true",
-		// 		removeRedundantAttributes: "true",
-		// 		sortAttributes: "true",
-		// 		sortClassName: "true",
-		// 		minifyCSS: "true",
-		// 		minifyJS: "true",
-		// 		useShortDoctype: "true",
-		// 		collapseBooleanAttributes: "true"
-		// }))
-		.pipe(gulp.dest("dist"));
+	gulp.src("app/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+	return (gulp
+			.src("app/*.html")
+			// .pipe(htmlmin({
+			// 		collapseWhitespace: "true",
+			// 		removeAttributeQuotes: "true",
+			// 		removeComments: "true",
+			// 		removeEmptyAttributes: "true",
+			// 		removeEmptyElements: "true",
+			// 		removeOptionalTags: "true",
+			// 		removeRedundantAttributes: "true",
+			// 		sortAttributes: "true",
+			// 		sortClassName: "true",
+			// 		minifyCSS: "true",
+			// 		minifyJS: "true",
+			// 		useShortDoctype: "true",
+			// 		collapseBooleanAttributes: "true"
+			// }))
+			.pipe(gulp.dest("dist")) );
 });
