7dd07629d9ab1889dfd7d1cf86bd2ecdee73532c
DiDex <wc34fun@mail.ru>
2018-06-29 01:19:46 +0800

    build:noimg

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
-"use strict";
-const gulp = require("gulp"),
-  sass = require("gulp-sass"),
-  concat = require("gulp-concat"),
-  csso = require("gulp-csso"),
+'use strict'
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  csso = require('gulp-csso'),
   // gulpcopy = require("gulp-copy"),
-  image = require("gulp-imagemin"),
-  uglify = require("gulp-uglifyjs"),
+  image = require('gulp-imagemin'),
+  uglify = require('gulp-uglifyjs'),
   // htmlmin = require("gulp-htmlmin"),
-  autoprx = require("gulp-autoprefixer"),
-  browser = require("browser-sync"),
+  autoprx = require('gulp-autoprefixer'),
+  browser = require('browser-sync'),
   // remember = require("gulp-remember"),
-  cached = require("gulp-cached"),
+  cached = require('gulp-cached'),
   // debug = require("gulp-debug"),
-  newer = require("gulp-newer"),
-  notify = require("gulp-notify"),
-  plumber = require("gulp-plumber"),
-  babel = require("gulp-babel"),
-  pug = require("gulp-pug"),
+  newer = require('gulp-newer'),
+  notify = require('gulp-notify'),
+  plumber = require('gulp-plumber'),
+  babel = require('gulp-babel'),
+  pug = require('gulp-pug'),
   // rename = require("gulp-rename"),
-  kraken = require("gulp-kraken"),
-  ts = require("gulp-typescript"),
-  sourcemaps = require("gulp-sourcemaps");
+  kraken = require('gulp-kraken'),
+  ts = require('gulp-typescript'),
+  sourcemaps = require('gulp-sourcemaps')
 
-gulp.task("libs:js", function() {
+gulp.task('libs:js', function() {
   return gulp
     .src([
-      "js/libs/jquery.js",
-      "js/libs/magnificPopup.js",
-      "js/libs/owlCarousel.js",
-      "js/libs/bootstrap/tab.js",
+      'js/libs/jquery.js',
+      'js/libs/magnificPopup.js',
+      'js/libs/owlCarousel.js',
+      'js/libs/bootstrap/tab.js',
       // "lib/jquery/dist/jquery.min.js",
       // "lib/materialize/materialize.min.js"
       // "lib/bootstrap/dist/js/bootstrap.min.js",
@@ -37,12 +37,12 @@ gulp.task("libs:js", function() {
       // "lib/waypoints/lib/noframework.waypoints.min.js",
       // "lib/magnific-popup/dist/jquery.magnific-popup.min.js"
     ])
-    .pipe(concat("libs.js"))
+    .pipe(concat('libs.js'))
     .pipe(uglify())
-    .pipe(gulp.dest("js/src"));
-});
+    .pipe(gulp.dest('js/src'))
+})
 
-gulp.task("libs:css", ["sass"], function() {
+gulp.task('libs:css', ['sass'], function() {
   return gulp
     .src([
       // "lib/magnific-popup/dist/magnific-popup.css",
@@ -52,184 +52,203 @@ gulp.task("libs:css", ["sass"], function() {
       // "lib/materialize/ghpages-materialize.css",
       // "lib/bootstrap/dist/css/bootstrap.css"
     ])
-    .pipe(concat("libs.css"))
+    .pipe(concat('libs.css'))
     .pipe(csso())
-    .pipe(gulp.dest("css"));
-});
+    .pipe(gulp.dest('css'))
+})
 
-gulp.task("libs", ["libs:css", "libs:js"], function() {
-  return;
-});
+gulp.task('libs', ['libs:css', 'libs:js'], function() {
+  return
+})
 
-gulp.task("sass", function() {
-  return gulp
-    .src("sass/*.sass")
+gulp.task('sass', function() {
+  return (
+    gulp
+      .src('sass/*.sass')
+      .pipe(
+        plumber({
+          errorHandler: notify.onError(err => ({
+            title: 'Sass',
+            message: err.message,
+          })),
+        }),
+      )
+      // .pipe(cached("sass"))
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(autoprx(['last 15 versions', '> 1%'], {cascade: true}))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('css/'))
+      .pipe(browser.reload({stream: true}))
+  )
+})
+
+gulp.task('ts', cb =>
+  gulp
+    .src('ts/**/*.ts')
     .pipe(
       plumber({
         errorHandler: notify.onError(err => ({
-          title: "Sass",
-          message: err.message
-        }))
-      })
-    )
-    // .pipe(cached("sass"))
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(autoprx(["last 15 versions", "> 1%"], { cascade: true }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("css/"))
-    .pipe(browser.reload({ stream: true }));
-});
-
-gulp.task("ts", cb =>
-  gulp
-    .src("ts/**/*.ts")
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(err => ({
-          title: "Ts",
-          message: err.message
-        }))
-      })
+          title: 'Ts',
+          message: err.message,
+        })),
+      }),
     )
     .pipe(sourcemaps.init())
-    .pipe(cached("ts"))
-    .pipe(ts({ noImplicitAny: true, outFile: "main.ts.js" }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("js/src"))
-);
+    .pipe(cached('ts'))
+    .pipe(ts({noImplicitAny: true, outFile: 'main.ts.js'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('js/src')),
+)
 
-gulp.task("js", cb =>
+gulp.task(
+  'js',
+  cb =>
+    gulp
+      .src('js/src/main.js')
+      .pipe(
+        plumber({
+          errorHandler: notify.onError(err => ({
+            title: 'Js',
+            message: err.message,
+          })),
+        }),
+      )
+      .pipe(cached('js'))
+      .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(concat('main.min.js'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('js/src')),
+  // .pipe(browser.reload({ stream: true }))
+)
+
+gulp.task('scripts', cb =>
   gulp
-    .src("js/src/main.js")
-    .pipe(
-      plumber({
-        errorHandler: notify.onError(err => ({
-          title: "Js",
-          message: err.message
-        }))
-      })
-    )
-    .pipe(cached("js"))
+    .src(['js/src/libs.js', 'js/src/main.min.js', 'js/src/main.ts.js'])
     .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(concat("main.min.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("js/src"))
-    // .pipe(browser.reload({ stream: true }))
-);
+    .pipe(concat('scripts.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('js'))
+    .pipe(browser.reload({stream: true})),
+)
 
-gulp.task("scripts", cb =>
-  gulp
-    .src(["js/src/libs.js","js/src/main.min.js", "js/src/main.ts.js"])
-    .pipe(sourcemaps.init())
-    .pipe(concat("scripts.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("js"))
-    .pipe(browser.reload({ stream: true }))
-);
-
-gulp.task("sass:stream", function() {
-  return sassruby("sass", { sourcemap: true })
-    .on("error", function(err) {
-      console.error("Error!", err.message);
+gulp.task('sass:stream', function() {
+  return sassruby('sass', {sourcemap: true})
+    .on('error', function(err) {
+      console.error('Error!', err.message)
     })
     .pipe(
-      sourcemap.write("./", {
+      sourcemap.write('./', {
         includeContent: false,
-        sourceRoot: "sass"
-      })
+        sourceRoot: 'sass',
+      }),
     )
-    .pipe(browserSync.stream({ match: "**/*.css" }));
-});
+    .pipe(browserSync.stream({match: '**/*.css'}))
+})
 
-gulp.task("browser", function() {
+gulp.task('browser', function() {
   browser({
     server: {
-      baseDir: "./"
+      baseDir: './',
     },
-    notify: false
-  });
-});
+    notify: false,
+  })
+})
 
-gulp.task("browser:dist", function() {
+gulp.task('browser:dist', function() {
   browser({
     server: {
-      baseDir: "dist"
+      baseDir: 'dist',
     },
-    notify: false
-  });
-});
+    notify: false,
+  })
+})
 
-gulp.task("pug", cb =>
+gulp.task('pug', cb =>
   gulp
-    .src("index.pug")
+    .src('index.pug')
     .pipe(
       plumber({
         errorHandler: notify.onError(err => ({
-          title: "Pug",
-          message: err.message
-        }))
-      })
+          title: 'Pug',
+          message: err.message,
+        })),
+      }),
     )
     // .pipe(cached("pug"))
     .pipe(pug({pretty: true}))
-    .pipe(gulp.dest("./"))
-);
+    .pipe(gulp.dest('./')),
+)
 
-gulp.task("watch", ["browser", "pug", "sass", "js", "ts", "scripts"], function() {
-  gulp.watch("sass/**/*.sass", ["sass"]);
-  gulp.watch("sass/**/*.scss", ["sass"]);
-  gulp.watch("*.html", browser.reload);
-  gulp.watch("js/**/*.js", ["js", "scripts"]);
-  gulp.watch("ts/**/*.ts", ["ts", "scripts"]);
-  gulp.watch("*.pug", ["pug"]);
-});
+gulp.task(
+  'watch',
+  ['browser', 'pug', 'sass', 'js', 'ts', 'scripts'],
+  function() {
+    gulp.watch('sass/**/*.sass', ['sass'])
+    gulp.watch('sass/**/*.scss', ['sass'])
+    gulp.watch('*.html', browser.reload)
+    gulp.watch('js/**/*.js', ['js', 'scripts'])
+    gulp.watch('ts/**/*.ts', ['ts', 'scripts'])
+    gulp.watch('*.pug', ['pug'])
+  },
+)
 
-gulp.task("watch:dist", ["build", "browser:dist"], function() {
-  gulp.watch("dist/*.html", browser.reload);
-});
+gulp.task('watch:dist', ['build', 'browser:dist'], function() {
+  gulp.watch('dist/*.html', browser.reload)
+})
 
-gulp.task("kraken", function() {
-  var config = {};
+gulp.task('kraken', function() {
+  var config = {}
   try {
-    config = require("./design/kraken.json");
+    config = require('./design/kraken.json')
     // watch for keys in another place ;)
   } catch (error) {
-    console.log("\n\n\n Missing kraken.json");
+    console.log('\n\n\n Missing kraken.json')
   }
 
-  gulp.src("dist/img/**/*").pipe(kraken(config));
-});
+  gulp.src('dist/img/**/*').pipe(kraken(config))
+})
 
-gulp.task("imagemin", function() {
+gulp.task('imagemin', function() {
   gulp
-    .src("img/**/*")
-    .pipe(newer("dist/img"))
+    .src('img/**/*')
+    .pipe(newer('dist/img'))
     .pipe(image())
-    .pipe(gulp.dest("dist/img"));
-});
+    .pipe(gulp.dest('dist/img'))
+})
 
-gulp.task("build", ["imagemin", "kraken"], function() {
+gulp.task('build:noimg', () => {
+  gulp.src('mail.php').pipe(gulp.dest('dist'))
   gulp
-    .src('mail.php')
-    .pipe(gulp.dest("dist"))
-  gulp
-    .src("css/**/*.css")
+    .src('css/**/*.css')
     .pipe(csso())
-    .pipe(gulp.dest("dist/css"));
+    .pipe(gulp.dest('dist/css'))
   gulp
-    .src("js/scripts.js")
+    .src('js/scripts.js')
     .pipe(uglify())
-    .pipe(gulp.dest("dist/js"));
+    .pipe(gulp.dest('dist/js'))
+  gulp.src('fonts/**/*').pipe(gulp.dest('dist/fonts'))
+})
+
+gulp.task('build', ['imagemin', 'kraken'], function() {
+  gulp.src('mail.php').pipe(gulp.dest('dist'))
+  gulp
+    .src('css/**/*.css')
+    .pipe(csso())
+    .pipe(gulp.dest('dist/css'))
+  gulp
+    .src('js/scripts.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
   // gulp
   //   .src("js/libs.js")
   //   .pipe(uglify())
   //   .pipe(gulp.dest("dist/js"));
-  gulp.src("fonts/**/*").pipe(gulp.dest("dist/fonts"));
+  gulp.src('fonts/**/*').pipe(gulp.dest('dist/fonts'))
   return (
     gulp
-      .src("*.html")
+      .src('*.html')
       // .pipe(htmlmin({
       // 		collapseWhitespace: "true",
       // 		removeAttributeQuotes: "true",
@@ -245,6 +264,6 @@ gulp.task("build", ["imagemin", "kraken"], function() {
       // 		useShortDoctype: "true",
       // 		collapseBooleanAttributes: "true"
       // }))
-      .pipe(gulp.dest("dist"))
-  );
-});
+      .pipe(gulp.dest('dist'))
+  )
+})
